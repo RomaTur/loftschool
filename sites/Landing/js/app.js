44bef4de9b28eb491c56df4d0414419bdbc0efe9
Roman <borka.grr@gmail.com>
2017-11-10 20:50:59 +0300

    training js

--- sites/Landing/js/app.js
+++ sites/Landing/js/app.js
@@ -1,3 +1,16 @@
 
-
+////////////ФУНКЦИОНАЛ/////////
 //headerMenuFullScreen
+let welcomeNavOpen = document.getElementById('welcomeNavOpen');
+let welcomeNavClose = document.getElementById('welcomeNavClose');
+let welcomeNavList = document.querySelector('.nav-burger__list');
+
+// console.log(welcomeNavOpen, welcomeNavClose, welcomeNavList);
+
+welcomeNavOpen.addEventListener('click', function(){
+    welcomeNavList.classList.add('nav-burger__list--active');
+});
+
+welcomeNavClose.addEventListener('click', function(){
+    welcomeNavList.classList.remove('nav-burger__list--active')
+});
