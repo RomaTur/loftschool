1307a89a59a6cc2e6585fb5c5007e9243e46af8d
Roman <borka.grr@gmail.com>
2017-11-12 17:39:11 +0300

    developing functions

--- sites/Landing/js/app.js
+++ sites/Landing/js/app.js
@@ -8,7 +8,70 @@ var welcomeNavList = document.querySelector('.nav-burger__list');
 
 welcomeNavOpen.addEventListener('click', function(elem){
     welcomeNavList.classList.add('nav-burger__list--active');
+    isScroll(false);
 });
 welcomeNavClose.addEventListener('click', function(){
     welcomeNavList.classList.remove('nav-burger__list--active');
+    isScroll(true);
 });
+
+
+//slider
+
+
+
+
+//accordeon
+
+var teamPersons = document.querySelectorAll('.team__person');
+
+for (var i = 0; i < teamPersons.length; i++) {
+    teamPersons[i].addEventListener('click', function(event){
+        for (var i = 0; i < teamPersons.length; i++) {
+            teamPersons[i].classList.remove('team__person--active');
+        }
+        if(event.target.parentNode.parentNode.classList.contains('team__person')){
+            event.target.parentNode.parentNode.classList.add('team__person--active');
+        }
+    });
+}
+
+//menu
+
+
+
+
+
+
+
+
+
+//функция разрешения/запрета скролла//
+function isScroll(bool){
+    document.onmousewheel=document.onwheel=function(){
+        if(bool==false) {
+            return false;
+        }
+    	else {
+    	    return true;
+    	}
+    };
+    document.addEventListener("MozMousePixelScroll",function(){
+        if(bool==false){
+            return false;
+        }
+        else {
+            return true;
+        }
+    },false);
+    document.onkeydown=function(e) {
+    	if (e.keyCode>=33&&e.keyCode<=40){
+            if (bool==false){
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+    }
+};
